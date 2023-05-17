2ef3862b65733e95b54dde47813c1e304653a3b8
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-17 14:43:27 +0300

    Levels: Add skeleton classes for level 3 bosses

---
+++ src/WizardGame2/GameObjects/BossBehaviours/CristianPhase2Behaviour.java
@@ -0,0 +1,25 @@
+package WizardGame2.GameObjects.BossBehaviours;
+
+import WizardGame2.GameObjects.Boss;
+import WizardGame2.Level;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+@SuppressWarnings("unused")
+public class CristianPhase2Behaviour implements Boss.Behaviour {
+    @Override
+    public void attachTo(Boss boss) {
+
+    }
+
+    @Override
+    public void render(Graphics gfx, BufferedImage sprite, int centerX, int centerY) {
+
+    }
+
+    @Override
+    public void update(Level level, long currentTime, int playerX, int playerY) {
+
+    }
+}
