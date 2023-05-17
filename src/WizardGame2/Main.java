9a45741e6493c5c08b1b0c145597407ab3c81992
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-17 21:38:32 +0300

    Add `IS_DEBUG_BUILD` feature flag

--- src/WizardGame2/Main.java
+++ src/WizardGame2/Main.java
@@ -1,6 +1,13 @@
 package WizardGame2;
 
 public class Main {
+    // Poor man's feature flags
+
+    /**
+     * Allows code to check if it's running in a debug build and act appropriately
+     */
+    public static final boolean IS_DEBUG_BUILD = true;
+
     public static void main(String[] args) {
         Game.getInstance().startGame();
     }
