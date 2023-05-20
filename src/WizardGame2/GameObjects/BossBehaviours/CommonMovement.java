4a656a01b877c102827c68d17623fe6f65609c27
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-20 22:46:36 +0300

    Bosses/CommonMovement: Clamp target coords to the world

--- src/WizardGame2/GameObjects/BossBehaviours/CommonMovement.java
+++ src/WizardGame2/GameObjects/BossBehaviours/CommonMovement.java
@@ -2,6 +2,7 @@ package WizardGame2.GameObjects.BossBehaviours;
 
 import WizardGame2.GameObjects.Boss;
 import WizardGame2.Level;
+import WizardGame2.Utils;
 
 import java.util.Random;
 
@@ -35,12 +36,18 @@ public class CommonMovement {
     }
 
     protected void performMovement(Level level, int playerX, int playerY) {
+        var rightBorder = level.getWidth() - 16;
+        var bottomBorder = level.getHeight() - 16;
+
         if ((targetX == -1 || isClose(targetX, boss.getX())) && playerX != 0) {
             targetX = playerX + horizontalOffsets[random.nextInt(0, horizontalOffsets.length)];
+            targetX = Utils.clamp(16, rightBorder, targetX);
         }
 
         if ((targetY == -1 || isClose(targetY, boss.getY())) && playerY != 0) {
             targetY = playerY + verticalOffsets[random.nextInt(0, verticalOffsets.length)];
+            //noinspection SuspiciousNameCombination
+            targetY = Utils.clamp(16, bottomBorder, targetY);
         }
 
         boss.moveToTarget(level, targetX, targetY);
