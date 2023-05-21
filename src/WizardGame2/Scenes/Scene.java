abc72634799530ee57afa930efd8aababd88f3c1
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-21 14:52:55 +0300

    Scenes: Add some docs

--- src/WizardGame2/Scenes/Scene.java
+++ src/WizardGame2/Scenes/Scene.java
@@ -6,9 +6,20 @@ import java.awt.*;
  * Interface defining the operations for a Game scene
  */
 public interface Scene {
+    /**
+     * Runs scene update code
+     * @param currentTime the current time in nanoseconds
+     * @return whether the {@link SceneManager} should progress to the next scene or not
+     */
     SceneUpdate update(long currentTime);
 
+    /**
+     * Renders the scene to the screen
+     */
     void render(Graphics gfx);
 
+    /**
+     * Returns the next scene the {@link SceneManager} should progress to
+     */
     Scene nextScene();
 }
--- src/WizardGame2/Scenes/SceneManager.java
+++ src/WizardGame2/Scenes/SceneManager.java
@@ -2,13 +2,24 @@ package WizardGame2.Scenes;
 
 import java.awt.*;
 
+/**
+ * The class that manages the scene-state machine of the game
+ */
 public class SceneManager {
     private Scene currentScene;
 
+    /**
+     * Creates a new {@link SceneManager}
+     * @param scene the initial scene
+     */
     public SceneManager(Scene scene) {
         currentScene = scene;
     }
 
+    /**
+     * Runs the update code of the current {@link Scene}
+     * @param currentTime the current time in nanoseconds
+     */
     public void update(long currentTime) {
         var result = currentScene.update(currentTime);
 
@@ -21,6 +32,9 @@ public class SceneManager {
         }
     }
 
+    /**
+     * Renders the current scene on the screen
+     */
     public void render(Graphics gfx) {
         currentScene.render(gfx);
     }
