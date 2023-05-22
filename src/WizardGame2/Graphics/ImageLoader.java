8dd21dff0cf84178532b3e73cbf68e41cecac44a
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-05-22 23:18:13 +0300

    ImageLoader: Update docs

--- src/WizardGame2/Graphics/ImageLoader.java
+++ src/WizardGame2/Graphics/ImageLoader.java
@@ -7,23 +7,17 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Objects;
 
-/*! \class public class ImageLoader
-    \brief Clasa ce contine o metoda statica pentru incarcarea unei imagini in memorie.
+/**
+ * Class that makes loading images from resources easier
  */
 public class ImageLoader {
-    /*! \fn public static BufferedImage loadImage(String path)
-        \brief Incarca o imagine intr-un obiect BufferedImage si returneaza o referinta catre acesta.
-
-        \param path Calea relativa pentru localizarea fisierul imagine.
+    /**
+     * Load an image into memory
+     * @param path resource root-relative path to the image
+     * @return the image, or null if it could not be loaded
      */
     public static BufferedImage loadImage(String path) {
-        /// Avand in vedere exista situatii in care fisierul sursa sa nu poate fi accesat
-        /// metoda read() arunca o excpetie ce trebuie tratata
         try {
-            /// Clasa ImageIO contine o serie de metode statice pentru file IO.
-            /// Metoda read() are ca argument un InputStream construit avand ca referinta
-            /// directorul res, director declarat ca director de resurse in care se gasesc resursele
-            /// proiectului sub forma de fisiere sursa.
             return ImageIO.read(Objects.requireNonNull(ImageLoader.class.getResource(path)));
         } catch (IOException e) {
             Utils.logException(ImageLoader.class, e, "got an IO Exception trying to load '%s'", path);
